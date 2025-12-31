96fb7cdfecbf0bad48d5ad7ce41eb56d38de7064
kirikiritanpo <121788724+kirikiritanpo@users.noreply.github.com>
2025-12-31 21:13:32 +0900

    like-buuton2

--- App.tsx
+++ App.tsx
@@ -1,21 +1,18 @@
 import "./App.css";
-import { useState } from "react";
-function App() {
-  return (
+import { useState } from "react" ;
+function App(){
+  return(
     <>
       <LikeButton />
     </>
   );
 }
-function LikeButton() {
+
+function LikeButton(){
   const [count, setCount] = useState(999);
   const handleClick = () => {
     setCount(count + 1);
   };
-  return (
-    <span className="likeButton" onClick={handleClick}>
-      ♥ {count}
-    </span>
-  );
+  return <span className="likeButton" onClick={handleClick}>♥{count}</span>;
 }
 export default App;
